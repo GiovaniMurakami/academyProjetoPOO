65c6de11c79a40ef2fb7e5b890f02a78ec599699
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-02 19:01:18 -0300

    added service Usuario

---
+++ src/main/java/br/com/academy/service/ServiceUsuario.java
@@ -0,0 +1,42 @@
+package br.com.academy.service;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.academy.dao.UsuarioDao;
+import br.com.academy.exceptions.CriptoExistException;
+import br.com.academy.exceptions.EmailExistsException;
+import br.com.academy.exceptions.ServiceExp;
+import br.com.academy.model.Usuario;
+import br.com.academy.utils.Hash;
+
+@Service
+public class ServiceUsuario {
+
+    @Autowired
+    private UsuarioDao usuarioRepositorio;
+
+    public void salvarUsuario(Usuario usuario) throws Exception {
+        if (usuarioRepositorio.findByEmail(usuario.getEmail()) != null) {
+            throw new EmailExistsException("Já existe um email cadastrado para: " + usuario.getEmail());
+        }
+
+        try {
+            usuario.setPassword(Hash.md5(usuario.getPassword()));
+        } catch (NoSuchAlgorithmException error) {
+            throw new CriptoExistException("Erro na criptografia da senha");
+        }
+
+        usuarioRepositorio.save(usuario);
+    }
+
+    public Usuario loginUser(String email, String senha) throws ServiceExp {
+        Usuario userLogin = usuarioRepositorio.findLoginByEmail(email, senha);
+        if (userLogin == null) {
+            throw new ServiceExp("Usuário não encontrado ou senha inválida");
+        }
+        return userLogin;
+    }
+}
