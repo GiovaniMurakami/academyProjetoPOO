b15ab3d1c804fec6bf57635676706d6f31581c9b
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-02 19:00:14 -0300

    added service exception

---
+++ src/main/java/br/com/academy/exceptions/ServiceExp.java
@@ -0,0 +1,11 @@
+package br.com.academy.exceptions;
+
+public class ServiceExp extends Exception {
+
+	public ServiceExp(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	private static final long serialVersionUID = 1L;
+}
