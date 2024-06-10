1d4a3de8d83af0d7cd1c498890d6d33cf736d296
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-10 08:42:16 -0300

    Added Javadoc Hash

--- src/main/java/br/com/academy/utils/Hash.java
+++ src/main/java/br/com/academy/utils/Hash.java
@@ -4,12 +4,23 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Classe utilitária para gerar hash MD5.
+ */
 public class Hash {
 
+	/**
+	 * Gera um hash MD5 para a senha fornecida.
+	 *
+	 * @param senha a senha a ser convertida em hash.
+	 * @return a representação hexadecimal do hash MD5 da senha.
+	 * @throws NoSuchAlgorithmException se o algoritmo de hash MD5 não for
+	 *                                  encontrado.
+	 */
 	public static String md5(String senha) throws NoSuchAlgorithmException {
 		MessageDigest messagedig = MessageDigest.getInstance("MD5");
-		BigInteger hash = new BigInteger(1, messagedig.digest(senha.getBytes()));
+		byte[] hashBytes = messagedig.digest(senha.getBytes());
+		BigInteger hash = new BigInteger(1, hashBytes);
 		return hash.toString(16);
 	}
-	
 }
