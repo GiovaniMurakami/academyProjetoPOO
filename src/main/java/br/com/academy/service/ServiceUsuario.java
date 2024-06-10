07211c08d04e63d4cb539bcc3b52de38bf10a94b
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-10 08:41:27 -0300

    added Javadoc ServiceUsuario

--- src/main/java/br/com/academy/service/ServiceUsuario.java
+++ src/main/java/br/com/academy/service/ServiceUsuario.java
@@ -12,12 +12,21 @@ import br.com.academy.exceptions.ServiceExp;
 import br.com.academy.model.Usuario;
 import br.com.academy.utils.Hash;
 
+/**
+ * Serviço para manipulação de operações relacionadas aos usuários.
+ */
 @Service
 public class ServiceUsuario {
 
     @Autowired
     private UsuarioDao usuarioRepositorio;
 
+    /**
+     * Salva um novo usuário.
+     *
+     * @param usuario o usuário a ser salvo.
+     * @throws Exception se ocorrer um erro durante o processo de salvamento.
+     */
     public void salvarUsuario(Usuario usuario) throws Exception {
         if (usuarioRepositorio.findByEmail(usuario.getEmail()) != null) {
             throw new EmailExistsException("Já existe um email cadastrado para: " + usuario.getEmail());
@@ -32,6 +41,14 @@ public class ServiceUsuario {
         usuarioRepositorio.save(usuario);
     }
 
+    /**
+     * Realiza o login do usuário.
+     *
+     * @param email o email do usuário.
+     * @param senha a senha do usuário.
+     * @return o usuário autenticado.
+     * @throws ServiceExp se ocorrer um erro durante o processo de login.
+     */
     public Usuario loginUser(String email, String senha) throws ServiceExp {
         Usuario userLogin = usuarioRepositorio.findLoginByEmail(email, senha);
         if (userLogin == null) {
