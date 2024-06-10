15f20346379b3d3d782d47ec1986253d85229d1e
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-10 08:40:00 -0300

    added Javadoc Interceptor

--- src/main/java/br/com/academy/interceptors/AuthenticationInterceptor.java
+++ src/main/java/br/com/academy/interceptors/AuthenticationInterceptor.java
@@ -7,11 +7,26 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+/**
+ * Interceptor para autenticação de usuários.
+ */
 @Component
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
+    /**
+     * Intercepta a requisição antes que o controlador seja chamado para verificar
+     * se o usuário está autenticado.
+     *
+     * @param request  a requisição HTTP.
+     * @param response a resposta HTTP.
+     * @param handler  o handler que está sendo executado.
+     * @return true se o usuário estiver autenticado ou se a requisição for para uma
+     *         rota pública, false caso contrário.
+     * @throws Exception se ocorrer um erro durante o processo de autenticação.
+     */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
         HttpSession session = request.getSession();
         String uri = request.getRequestURI();
 
