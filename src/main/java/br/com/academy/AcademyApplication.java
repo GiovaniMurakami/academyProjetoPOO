db1f38d4f9764a5f4cdb80b90b7556f9882abfbd
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-10 08:43:08 -0300

    added Javadoc AcademyApplication

--- src/main/java/br/com/academy/AcademyApplication.java
+++ src/main/java/br/com/academy/AcademyApplication.java
@@ -3,11 +3,18 @@ package br.com.academy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal responsável por iniciar a aplicação Spring Boot.
+ */
 @SpringBootApplication
 public class AcademyApplication {
 
+	/**
+	 * Método principal que inicia a aplicação Spring Boot.
+	 *
+	 * @param args argumentos de linha de comando (não utilizado neste caso).
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AcademyApplication.class, args);
 	}
-
 }
