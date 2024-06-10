05143b2c1daa231bcba78b1312beb6c04bbe2d9f
Giovani Murakami <giovani.murakami@outlook.com>
2024-06-10 08:37:13 -0300

    added Javadoc UsuarioDao

--- src/main/java/br/com/academy/dao/UsuarioDao.java
+++ src/main/java/br/com/academy/dao/UsuarioDao.java
@@ -6,10 +6,30 @@ import org.springframework.stereotype.Repository;
 
 import br.com.academy.model.Usuario;
 
+/**
+ * Interface para manipulação de operações relacionadas aos usuários no banco de
+ * dados.
+ */
 @Repository
 public interface UsuarioDao extends CrudRepository<Usuario, Long> {
+
+    /**
+     * Busca um usuário pelo e-mail.
+     *
+     * @param email o e-mail do usuário a ser buscado.
+     * @return o usuário encontrado, ou null se nenhum usuário for encontrado com o
+     *         e-mail fornecido.
+     */
     Usuario findByEmail(String email);
-    
+
+    /**
+     * Busca um usuário pelo e-mail e senha.
+     *
+     * @param email    o e-mail do usuário.
+     * @param password a senha do usuário.
+     * @return o usuário encontrado, ou null se nenhum usuário for encontrado com o
+     *         e-mail e senha fornecidos.
+     */
     @Query("SELECT u FROM Usuario u WHERE u.email = :email AND u.password = :password")
     Usuario findLoginByEmail(String email, String password);
 }
